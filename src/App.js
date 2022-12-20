d43317bea212265a0f3dade59f11e0382e91695a
rajp6265 <rajp6265.rp@gmail.com>
2022-12-20 00:58:18 -0500

    first push

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import "./App.css";
+import React from "react";
+import { CardList } from "./components/card-list/card-list.components";
+import { SearchBox } from "./components/search-box/search-box.component";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField:''
+    };
+    // this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField:e.target.value})
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  render() {
+
+    const {monsters , searchField} = this.state;
+    const filtteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
+
+    return (
+      
+      <div className="App">
+        <h1>Monster Rolodex</h1>
+        <SearchBox
+        placeholder='Search Monsters'
+        handleChange= {this.handleChange}
+        />
+        <CardList monsters={filtteredMonsters}> </CardList>
+      </div>
+    );
+  }
+}
+
+export default App;
